bb7e76188066bec474e8a94a84d022ae273a8738
OMAR-EHAB777 <ramomixes@hotmail.com>
2019-06-23 11:39:04 +0200

    fixing carousel slider bugs

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,17 @@
 $(function () {
       // custom formatting example
       "use strict";
-      $('.timer').countTo();
+      $('.timer').countTo({
+        interval:15000
+      });
+
 });
  $('.dropdown').hover(function () {
           $(this)('ul').slideToggle(400)
 
       });
+$(document).ready(function(){
+  $(".carouselExampleIndicators").carousel({
+      interval: 2000
+  });
+});
