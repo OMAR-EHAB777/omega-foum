1b2852d7b2a0bd5044e5569e3de2b495502cd383
OMAR-EHAB777 <ramomixes@hotmail.com>
2019-07-07 14:46:52 +0200

    adding django-heroku

--- omega/settings.py
+++ omega/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -143,3 +143,8 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+
+
+
+
+django_heroku.settings(locals())
